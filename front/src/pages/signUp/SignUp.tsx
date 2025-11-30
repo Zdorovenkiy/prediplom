b75f184c666c4352982d9997aac2f8d27f952f3c
namytsykov <72704139+Zdorovenkiy@users.noreply.github.com>
2025-12-01 00:19:06 +0300

    Авторизация

--- front/src/pages/signUp/SignUp.tsx
+++ front/src/pages/signUp/SignUp.tsx
@@ -1,7 +1,82 @@
+import { ColorsEnum } from "@/constants/colors/ColorsEnum"
+import { StyleList } from "@/constants/styles/StyleList"
+import CustomInput from "@/features/customInput/CustomInput"
+import { RoutePath } from "@/routers/config/routeConfig"
+import { Box, Button, Typography } from "@mui/material"
+
 type Props = {}
 
 export default function SignUp({}: Props) {
-  return (
-    <div>SignUp</div>
-  )
+    return (
+        <Box className="AuthPage" sx={StyleList.pages}>
+            <Box className="container" sx={StyleList.pagesContainer}>
+                
+                <Box 
+                    className="auth"       
+                    component="form"
+                    noValidate
+                    autoComplete="off"
+                    sx={StyleList.form}
+                    >
+                        <Typography variant="h5">
+                            Регистрация
+                        </Typography>
+                        <CustomInput 
+                            id='surname' 
+                            label='Фамилия'
+                            full={true}
+                            borderless={true}
+                            type="text"
+                            required
+                            />
+                        <CustomInput 
+                            id='name' 
+                            label='Имя'
+                            full={true}
+                            borderless={true}
+                            type="text"
+                            required
+                            />
+                        <CustomInput 
+                            id='patronymic' 
+                            label='Отчество'
+                            full={true}
+                            borderless={true}
+                            type="text"
+                            />
+                        <CustomInput 
+                            id='date' 
+                            label=''
+                            full={true}
+                            borderless={true}
+                            type="date"
+                            required
+                            />
+                        <CustomInput 
+                            id='email' 
+                            label='Email'
+                            full={true}
+                            borderless={true}
+                            type="email"
+                            required
+                            />
+                        <CustomInput 
+                            id='password' 
+                            label='Пароль'
+                            full={true}
+                            borderless={true}
+                            type="password"
+                            required
+                            />
+                        
+                        <Button 
+                            variant="contained"
+                            sx={{bgcolor: ColorsEnum.SECONDARY_BG_LIGHT}}
+                        >
+                            Отправить
+                        </Button>
+                </Box>
+            </Box>
+        </Box>
+    )
 }
