4846f46f041063c305247e6e5b14bf09f7c1e341
namytsykov <72704139+Zdorovenkiy@users.noreply.github.com>
2025-12-03 15:47:04 +0300

    Подключение auth

--- front/src/pages/auth/Auth.tsx
+++ front/src/pages/auth/Auth.tsx
@@ -2,14 +2,77 @@ import { ColorsEnum } from "@/constants/colors/ColorsEnum"
 import { StyleList } from "@/constants/styles/StyleList"
 import CustomBreadcrumbs from "@/features/customBreadcrumbs/CustomBreadcrumbs"
 import CustomInput from "@/features/customInput/CustomInput"
+import { useAuthMutation } from "@/globalState/model/user/api/userApi"
+import type { IUser } from "@/globalState/model/user/types/userType"
 import { useNavigation } from "@/hooks/UseNavigation"
 import { AppRoutes, RoutePath } from "@/routers/config/routeConfig"
 import { Box, Button, TextField, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
 
 type Props = {}
 
 export default function Auth({}: Props) {
     const navigate = useNavigation();
+
+    const [auth, { isSuccess, isError, isLoading, message }] = useAuthMutation<{
+        message: string;
+        isSuccess: boolean;
+        isError: boolean;
+        isLoading: boolean;
+    }>();
+
+    const [formData, setFormData] = useState<IUser>({
+        email: '',
+        password: ''
+    });
+  
+    const handleInputChange = (field: keyof IUser, value: string) => {
+        setFormData(prev => ({
+        ...prev,
+        [field]: value
+    }))};
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        try {
+        e.preventDefault();
+        
+        const authData = {
+            email: formData.email,
+            password: formData.password
+        };
+        
+        const response = await auth(authData);
+        
+        if (response.data?.role_id) {
+            localStorage.setItem('token', String(response.data.role_id));
+            localStorage.setItem('id', String(response.data.id));
+        }
+        
+        setFormData({
+            email: '',
+            password: ''
+        });
+        } catch (error: any) {
+            const errorMessage = error?.data?.message || 'Произошла ошибка при регистрации';
+            console.log(errorMessage);
+        }
+    };
+
+    useEffect(() => {
+        if (isSuccess) {
+        const timer = setTimeout(() => {
+            navigate(RoutePath.main);
+        }, 3000);
+        
+        return () => clearTimeout(timer);
+        }
+    }, [isSuccess, navigate]);
+
+    useEffect(() => {
+        if (isError) {
+            console.log("message", message);
+        }
+    }, [isError]);
 
     return (
         <Box className="AuthPage" sx={StyleList.pages}>
@@ -21,6 +84,7 @@ export default function Auth({}: Props) {
                     noValidate
                     autoComplete="off"
                     sx={StyleList.form}
+                    onSubmit={handleSubmit}
                     >
                         <Typography variant="h5">
                             Авторизация
@@ -31,6 +95,8 @@ export default function Auth({}: Props) {
                             full={true}
                             borderless={true}
                             type="email"
+                            value={formData.email}
+                            onChange={(e) => handleInputChange('email', e.target.value)}
                             />
 
                         <CustomInput 
@@ -39,11 +105,15 @@ export default function Auth({}: Props) {
                             full={true}
                             borderless={true}
                             type="password"
+                            value={formData.password}
+                            onChange={(e) => handleInputChange('password', e.target.value)}
                             />
                         
                         <Button 
                             variant="contained"
                             sx={{bgcolor: ColorsEnum.SECONDARY_BG_LIGHT}}
+                            type="submit"
+                            disabled={isLoading}
                         >
                             Отправить
                         </Button>
